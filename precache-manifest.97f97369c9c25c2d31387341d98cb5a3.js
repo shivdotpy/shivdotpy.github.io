3ff3d69f356f1ec6651549fdf57c212594d93350
shiv <shiv@revinfotech.com>
2018-12-24 17:47:34 +0530

    intro

---
+++ precache-manifest.97f97369c9c25c2d31387341d98cb5a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "96acc0106baf8b3f00dcb8c54cfc7b05",
+    "url": "/static/media/archi2.96acc010.jpg"
+  },
+  {
+    "revision": "840f47ce6702711db528b0e520c750b5",
+    "url": "/static/media/archi.840f47ce.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bf06907ce946b6c90bec",
+    "url": "/static/js/main.bf06907c.chunk.js"
+  },
+  {
+    "revision": "701e80665e0aa5267d43",
+    "url": "/static/js/1.701e8066.chunk.js"
+  },
+  {
+    "revision": "bf06907ce946b6c90bec",
+    "url": "/static/css/main.25f43f8f.chunk.css"
+  },
+  {
+    "revision": "e05dc15ca988841f1d432fbf9876dd82",
+    "url": "/index.html"
+  }
+];
